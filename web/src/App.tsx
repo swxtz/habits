6262f337dd3d8ee539791f9c00b5a313bfc6d0e4
Gustavo Mendonça <alf4r6@gmail.com>
2023-01-18 09:50:08 -0300

    web: react setup with vite

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,14 @@
+import { Habit } from "./components/Habit/Habit";
 import "./styles/global.css";
 
 export function App() {
   return (
-    <h1>Ola mundo</h1>
+    <div>
+      <Habit completed={1} />
+      <Habit completed={2} />
+      <Habit completed={3} />
+      <Habit completed={4} />
+      <Habit completed={5} />
+    </div>
   )
 }
