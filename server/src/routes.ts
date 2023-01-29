de1b721074420f0ed13306e423cc5e9c2a606005
Gustavo Mendonça <alf4r6@gmail.com>
2023-01-29 15:33:56 -0300

    server: create route get habits per day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -28,4 +28,48 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request) => {
+    const getDaysParams = z.object({
+      date: z.coerce.date()
+    });
+
+    const { date } = getDaysParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      }
+    });
+
+    const day = await prisma.day.findFirst({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true
+      }
+    });
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits
+    };
+  });
 }
+
